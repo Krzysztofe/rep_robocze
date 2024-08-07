4a3001e779f38a89950284d3a40d1858f0f044ce
krzysztofee <gitaragra@gmail.com>
2024-08-07 10:58:50 +0200

    Add belt interval

--- quiz/quiz.js
+++ quiz/quiz.js
@@ -29,7 +29,6 @@ class Quiz {
       return new Question(question[0], question[1], question[2]);
     });
 
-    console.log("", this.#questions);
     this.#currentQuestionIdx = 0;
     this.#score = 0;
   }
@@ -37,10 +36,69 @@ class Quiz {
     const question = document.getElementById("question");
     const answers = document.getElementById("answers");
     question.textContent = this.#questions[this.#currentQuestionIdx].title;
-    
   }
 }
 
 const tt = new Quiz(data);
 tt.displayQuestions();
-// console.log('',tt)
+
+const timerEl = document.getElementById("timer");
+const timeEendEl = document.getElementById("timerEnd");
+const timePrint = document.getElementById("timePrint");
+
+let intervalId;
+
+const addSecont = counter => {
+  counter.value++;
+  timePrint.innerText = counter.value;
+};
+
+let counter = { value: 0 };
+const handleTimerStart = () => {
+  intervalId = setInterval(() => addSecont(counter), 1000);
+};
+
+const handleTimerEnd = () => {
+  clearInterval(intervalId);
+};
+
+timerEl.addEventListener("click", handleTimerStart);
+timeEendEl.addEventListener("click", handleTimerEnd);
+const divEL = document.createElement("div");
+
+const print = () => {
+  const bodyEl = document.querySelector("body");
+  divEL.innerText = "yyy";
+  bodyEl.append(divEL);
+};
+
+const clear = () => {
+  divEL.remove();
+};
+
+setTimeout(print, 2000);
+setTimeout(clear, 5000);
+
+const nameElems = document.querySelectorAll(".name");
+
+// Function to print name
+const printName = nameEl => {
+  nameElems.forEach(el => el.classList.remove("animation"));
+  nameEl.classList.add("animation");
+};
+
+// Function to handle the print name action
+const handlePrintName = () => {
+  let currentIndex = 0;
+
+  // console.log('',currentIndex)
+
+  setInterval(() => {
+    printName(nameElems[currentIndex]);
+    currentIndex = (currentIndex + 1) % nameElems.length;
+      console.log("", currentIndex);
+  }, 4000);
+};
+
+// Add event listener for the window load event
+window.addEventListener("load", handlePrintName);
